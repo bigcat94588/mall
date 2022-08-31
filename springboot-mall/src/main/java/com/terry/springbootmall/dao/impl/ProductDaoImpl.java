57bc1504a9dfd1ba4a7558bf2cf707ae966bbded
bigcat94588 <bigcat94588@gmail.com>
2022-08-31 14:15:48 +0800

    商品功能-提煉程式

--- springboot-mall/src/main/java/com/terry/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/terry/springbootmall/dao/impl/ProductDaoImpl.java
@@ -29,17 +29,20 @@ public class ProductDaoImpl implements ProductDao {
 
         String sql = "SELECT count(*) FROM product WHERE 1=1";
 
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
 
         //查詢條件 Filtering
-        if (productQueryParams.getCategory()!=null){
-            sql = sql + " AND category=:category";
-            map.put("category",productQueryParams.getCategory().name()); //用name方法將Enum類型轉為字串
-        }
-        if (productQueryParams.getSearch()!=null){
-            sql = sql + " AND product_name LIKE :search";
-            map.put("search","%" + productQueryParams.getSearch() + "%"); //springboot模糊查詢要寫在map裡面，不能寫在sql語句裡面
-        }
+//        if (productQueryParams.getCategory() != null) {
+//            sql = sql + " AND category=:category";
+//            map.put("category", productQueryParams.getCategory().name()); //用name方法將Enum類型轉為字串
+//        }
+//        if (productQueryParams.getSearch() != null) {
+//            sql = sql + " AND product_name LIKE :search";
+//            map.put("search", "%" + productQueryParams.getSearch() + "%"); //springboot模糊查詢要寫在map裡面，不能寫在sql語句裡面
+//        }
+
+        sql = addFilteringSql(sql,map,productQueryParams);
+
         Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
 
         return total;
@@ -47,42 +50,45 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
-        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, "+
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, " +
                 "created_date, last_modified_date FROM product WHERE 1 = 1";
 
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
 
         //查詢條件 Filtering
-        if (productQueryParams.getCategory()!=null){
-            sql = sql + " AND category=:category";
-            map.put("category",productQueryParams.getCategory().name()); //用name方法將Enum類型轉為字串
-        }
-        if (productQueryParams.getSearch()!=null){
-            sql = sql + " AND product_name LIKE :search";
-            map.put("search","%" + productQueryParams.getSearch() + "%"); //springboot模糊查詢要寫在map裡面，不能寫在sql語句裡面
-        }
+//        if (productQueryParams.getCategory() != null) {
+//            sql = sql + " AND category=:category";
+//            map.put("category", productQueryParams.getCategory().name()); //用name方法將Enum類型轉為字串
+//        }
+
+//        if (productQueryParams.getSearch() != null) {
+//            sql = sql + " AND product_name LIKE :search";
+//            map.put("search", "%" + productQueryParams.getSearch() + "%"); //springboot模糊查詢要寫在map裡面，不能寫在sql語句裡面
+//        }
+
+        sql = addFilteringSql(sql,map,productQueryParams);
 
         //排序 Sorting
-        sql = sql + " ORDER BY "+productQueryParams.getOrderBy() + " "+ productQueryParams.getSort();
+        sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
 
         //分頁 Pagination
         sql = sql + " LIMIT :limit OFFSET :offset";
-        map.put("limit",productQueryParams.getLimit());
-        map.put("offset",productQueryParams.getOffset());
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowmapper());
 
-            return productList;  //RESTful設計理念，不去判斷是否有商品存
+        return productList;  //RESTful設計理念，不去判斷是否有商品存
     }
 
     @Override
     public Product getProductById(Integer productId) {
-        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, "+
-        "created_date, last_modified_date FROM product WHERE product_id = :productId";
-        Map<String,Object> map = new HashMap<>();
-        map.put("productId",productId);
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, " +
+                "created_date, last_modified_date FROM product WHERE product_id = :productId";
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowmapper());
-        if (productList.size()>0){
+        if (productList.size() > 0) {
             return productList.get(0);
         } else {
             return null;
@@ -91,44 +97,44 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public Integer createProduct(ProductRequest productRequest) {
-        String sql = "INSERT INTO product (product_name, category, image_url, price, stock, description, "+
-                "created_date, last_modified_date) VALUES (:productName, :category, :imageUrl, :price, :stock,"+
+        String sql = "INSERT INTO product (product_name, category, image_url, price, stock, description, " +
+                "created_date, last_modified_date) VALUES (:productName, :category, :imageUrl, :price, :stock," +
                 ":description, :createDate, :lastModifiedDate)";
 
-        Map<String,Object> map = new HashMap<>();
-        map.put("productName",productRequest.getProductName());
-        map.put("category",productRequest.getCategory().toString());
-        map.put("imageUrl",productRequest.getImageUrl());
-        map.put("price",productRequest.getPrice());
-        map.put("stock",productRequest.getStock());
-        map.put("description",productRequest.getDescription());
+        Map<String, Object> map = new HashMap<>();
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
 
         Date now = new Date();
-        map.put("createDate",now);
-        map.put("lastModifiedDate",now);
+        map.put("createDate", now);
+        map.put("lastModifiedDate", now);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
         int productId = keyHolder.getKey().intValue();
-        return  productId;
+        return productId;
     }
 
     @Override
     public void updateProduct(Integer productId, ProductRequest productRequest) {
-        String sql = "UPDATE product SET product_name = :productName, category =:category, image_url = :imageUrl,"+
-                     "stock = :stock, description = :description,last_modified_date = :lastModifiedDate"+
-                     " WHERE product_id = :productId";
+        String sql = "UPDATE product SET product_name = :productName, category =:category, image_url = :imageUrl," +
+                "stock = :stock, description = :description,last_modified_date = :lastModifiedDate" +
+                " WHERE product_id = :productId";
 
-        Map<String,Object> map = new HashMap<>();
-        map.put("productId",productId);
-        map.put("productName",productRequest.getProductName());
-        map.put("category",productRequest.getCategory().toString());
-        map.put("imageUrl",productRequest.getImageUrl());
-        map.put("price",productRequest.getPrice());
-        map.put("stock",productRequest.getStock());
-        map.put("description",productRequest.getDescription());
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
 
-        map.put("lastModifiedDate",new Date());
+        map.put("lastModifiedDate", new Date());
 
         namedParameterJdbcTemplate.update(sql, map);
     }
@@ -136,9 +142,23 @@ public class ProductDaoImpl implements ProductDao {
     @Override
     public void deleteProductById(Integer productId) {
         String sql = "DELETE FROM product WHERE product_id = :productId";
-        Map<String,Object> map = new HashMap<>();
-        map.put("productId",productId);
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
 
         namedParameterJdbcTemplate.update(sql, map);
+    }
+
+    private String addFilteringSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams) {
+        //查詢條件 Filtering
+        if (productQueryParams.getCategory() != null) {
+            sql = sql + " AND category=:category";
+            map.put("category", productQueryParams.getCategory().name()); //用name方法將Enum類型轉為字串
+        }
+        if (productQueryParams.getSearch() != null) {
+            sql = sql + " AND product_name LIKE :search";
+            map.put("search", "%" + productQueryParams.getSearch() + "%"); //springboot模糊查詢要寫在map裡面，不能寫在sql語句裡面
+        }
+        return sql;
+
     }
 }
